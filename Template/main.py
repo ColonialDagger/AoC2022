bea551b7fb49bba6c47365bbddf0b6b747ccd0d0
ColonialDagger <dannyg305@hotmail.com>
2022-11-28 02:20:09 -0500

    Template added

---
+++ Template/main.py
@@ -0,0 +1,17 @@
+def part1(data):
+    return
+
+def part2(data):
+    return
+
+if __name__ == '__main__':
+    testing = False
+
+    # Reads lines into a list input
+    print('Counting data...')
+    file = 'testinput.txt' if testing else 'input.txt'
+    with open(file) as f:
+        data = f.readlines()
+
+    print("Part 1 Solution: ")
+    print("Part 2 Solution: ")
