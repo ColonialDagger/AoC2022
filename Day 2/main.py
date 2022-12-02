fc8f837f7273d2adfc0c7d8609e62846cf25edf5
ColonialDagger <dannyg305@hotmail.com>
2022-12-01 21:42:50 -0500

    Created Day 2

---
+++ Day 2/main.py
@@ -0,0 +1,21 @@
+def solve(data):
+    # Part 1
+    ans1 = None
+
+    # Part 2
+    ans2 = None
+
+    return ans1, ans2
+
+if __name__ == '__main__':
+    testing = False
+
+    # Reads lines into a list input
+    print('Counting data...')
+    file = 'testinput.txt' if testing else 'input.txt'
+    with open(file) as f:
+        data = f.readlines()
+
+    ans = solve(data)
+    print(f"Part 1 Solution: {ans[0]}")
+    print(f"Part 2 Solution: {ans[1]}")
