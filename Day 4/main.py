1552dc8fe22cbda5d44153a6514c312d4ed7df68
ColonialDagger <dannyg305@hotmail.com>
2022-12-04 00:31:27 -0500

    Solved Day 4

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
 def solve(data):
-    # Part 1
-    ans1 = None
+    ans1 = 0
+    ans2 = 0
+    for i in data:
+        i = i.split(',')
+        i[0] = list(map(int, i[0].split('-')))
+        i[1] = list(map(int, i[1].split('-')))
 
-    # Part 2
-    ans2 = None
+        # Part 1
+        if (
+            (i[0][0] <= i[1][0] and i[0][1] >= i[1][1]) or
+            (i[1][0] <= i[0][0] and i[1][1] >= i[0][1])
+        ):
+            ans1 += 1
+
+        # Part 2
+        if (
+            i[0][0] <= i[1][0] <= i[0][1] or
+            i[0][0] <= i[1][1] <= i[0][1] or
+            i[1][0] <= i[0][1] <= i[1][1] or
+            i[1][0] <= i[0][1] <= i[1][1]
+        ):
+            ans2 += 1
 
     return ans1, ans2
 
-
 if __name__ == '__main__':
     testing = False
-
     # Reads lines into a list input
     print('Counting data...')
     file = 'testinput.txt' if testing else 'input.txt'
